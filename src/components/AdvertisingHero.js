c16b8465179124989d88006fa9ed278ba1e149ba
gagansaini1212 <gagansaini1212@gmail.com>
2019-12-02 20:19:17 +0530

    Change text size - Advertising page #34 fixed

--- src/components/AdvertisingHero.js
+++ src/components/AdvertisingHero.js
@@ -23,7 +23,7 @@ class Advertising extends React.Component {
           <section className="hero">
             <div className="hero-body">
               <div className="container">
-                <h1 className="title is-3">{adv.title}</h1>
+                <h1 className="subtitle is-6">{adv.title}</h1>
                 <h5 className="subtitle is-6">
                   {adv.introParagrpah.introParagrpah}
                 </h5>
